bd54377544009df23f77b66be99fe6be485687c6
Philipp Hugenroth <>
2024-07-03 08:36:09 +0200

    feat: initial commit
    Adds a rudimentary Spring project structure to build on in future commits.

---
+++ src/main/java/phugen/slotmachine/SlotmachineApplication.java
@@ -0,0 +1,13 @@
+package phugen.slotmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotmachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotmachineApplication.class, args);
+	}
+
+}
